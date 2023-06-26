5015597e53fb3070c2eee7ee56049e7cb502038a
XrerXrerX <50944603+XrerXrerX@users.noreply.github.com>
2023-06-26 21:21:10 +0700

    edit skills

--- app/partial/Skills.tsx
+++ app/partial/Skills.tsx
@@ -40,7 +40,7 @@ export default function Skills({ }: Props) {
 
             <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>hover over a skill for seing profiency</h3>
 
-            <div className='grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5 xl:gap-5 gap-2 md:gap-5'>
+            <div className='grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5  gap-2 md:gap-5 xl:gap-5'>
                 <SkillApache />
                 <SkillJs />
                 <SkillBoostrap />
