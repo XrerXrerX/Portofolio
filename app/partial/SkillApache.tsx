63543a38b5195cdd4f55ea5015008dfe1437e951
XrerXrerX <50944603+XrerXrerX@users.noreply.github.com>
2023-06-26 21:30:58 +0700

    edit skills

--- app/partial/SkillApache.tsx
+++ app/partial/SkillApache.tsx
@@ -14,7 +14,7 @@ export default function SkillApache({ directionLeft }: Props) {
                 transition={{ duration: 0.5 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 src="apache.jpg"
-                className='rounded-full boreder border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 filter group-hover:grayscale-0 transition duration-250 ease-in-out'
+                className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 filter group-hover:grayscale-0 transition duration-250 ease-in-out'
             />
             <div className=' absolute opacity-0 group-hover:opacity-80 tramsotopm duration-300 ease-in-out group-hover:bg-black w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 rounded-full'>
                 <div className='flex items-center justify-center h-full'>
--- app/partial/Skills.tsx
+++ app/partial/Skills.tsx
@@ -35,7 +35,7 @@ export default function Skills({ }: Props) {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
 
-            className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] px:10 xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+            className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] px:10 xl:px-10 min-h-screen justify-evenly xl:space-y-0 mx-auto items-center'>
             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
 
             <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>hover over a skill for seing profiency</h3>
