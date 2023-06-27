39c9fd51411ca8463d46b568312f78f112a3995f
XrerXrerX <50944603+XrerXrerX@users.noreply.github.com>
2023-06-27 11:38:40 +0700

    edit skills

--- app/partial/Experience.tsx
+++ app/partial/Experience.tsx
@@ -17,7 +17,7 @@ export default function Experience({ }: Props) {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
             className='h-screen flex relative overflow-hidden font-semibold flex-col text-left md:flex-row max-w-full px-5 justify-evenly mx-auto items-center'>
-            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
+            <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
 
             <div className='w-full flex space-x-5 overflow-x-scroll p-10 mt-20 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
                 <ExperienceCard />
--- app/partial/Skills.tsx
+++ app/partial/Skills.tsx
@@ -41,26 +41,26 @@ export default function Skills({ }: Props) {
             <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>hover over a skill for seing profiency</h3>
 
             <div className='grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5  gap-2 md:gap-5 xl:gap-5'>
-                <SkillApache />
-                <SkillJs />
-                <SkillBoostrap />
-                <SkillCss />
-                <SkillCoulflare />
+                <Skill />
                 <SkillUbuntu />
-                <SkillAws />
-                <SkillNginx />
+                <SkillMysql />
+                <SkillReact />
+                <SkillTypescript />
                 <SkillJavascript />
                 <SkillExpress />
                 <SkillNodejs />
-                <SkillTypescript />
-                <SkillCi />
-                <Skill />
-                <SkillReact />
-                <SkillNextjs />
                 <SkillMongodb />
-                <SkillSass />
-                <SkillMysql />
+                <SkillCoulflare />
+                <SkillApache />
+                <SkillAws />
+                <SkillNextjs />
+                <SkillCi />
                 <SkillTailwind />
+                <SkillNginx />
+                <SkillBoostrap />
+                <SkillCss />
+                <SkillJs />
+                <SkillSass />
             </div>
         </motion.div>
 
